f21d1617f64a88e2c7b71add28fed8b479d622cb
Lisa Balbach <lisajbalbach@gmail.com>
2020-10-29 21:53:57 -0400

    Update Orders.sql

--- Orders.sql
+++ Orders.sql
@@ -1,7 +1,8 @@
-﻿CREATE TABLE Orders(
-	OrderID int NOT NULL,
+CREATE TABLE Orders(
+	[OrdersID] int NOT NULL,
 	CustomerID int NOT NULL,
 	OrderDate datetime NULL,
 	FreightCharge money NULL,
 	TotalDue money NULL,
- CONSTRAINT PK_orders PRIMARY KEY CLUSTERED (OrderID));
+ CONSTRAINT PK_orders PRIMARY KEY CLUSTERED ([OrdersID]),
+ CONSTRAINT FK_OrdersCustomer FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID));
