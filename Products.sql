4c38d22b4b198dfc850a31a8c665979dcda368c2
Lisa Balbach <lisajbalbach@gmail.com>
2020-10-29 21:51:40 -0400

    Update Products.sql

--- Products.sql
+++ Products.sql
@@ -1,5 +1,5 @@
-﻿CREATE TABLE Products(
-	[ProductID] int NOT NULL,
-	ItemName nvarchar(50) NULL,
-	ItemDescription nvarchar(255) NULL,
- CONSTRAINT PK_Products PRIMARY KEY CLUSTERED ([ProductID]));
+CREATE TABLE Products(
+	[ProductsID] int NOT NULL,
+	[Name] nvarchar(50) NULL,
+	[Description] nvarchar(255) NULL,
+ CONSTRAINT PK_Products PRIMARY KEY CLUSTERED ([ProductsID]));
