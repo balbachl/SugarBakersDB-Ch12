6c34c449f40ac0c3c2db052d855519c0ca30b29c
Lisa Balbach <lisajbalbach@gmail.com>
2020-10-29 21:52:19 -0400

    Delete Customer.sql

--- Customer.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE Customer(
-	CustomerID int NOT NULL,
-	FirstName nvarchar(30) NOT NULL,
-	LastName nvarchar(50) NOT NULL,
-	[Address] nvarchar(100) NULL,
-	[ZipcodeID] nvarchar(20) NULL,
-	PhoneNumber nvarchar(30) NULL,
-	Extension nvarchar(10) NULL,
- CONSTRAINT PK_customer PRIMARY KEY CLUSTERED (CustomerID));
