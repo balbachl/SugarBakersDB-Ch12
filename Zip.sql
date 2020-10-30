23c3c9f549deca59cc85b9d4aa7eb8306ea20a08
Lisa Balbach <lisajbalbach@gmail.com>
2020-10-29 21:54:51 -0400

    Update Zip.sql

--- Zip.sql
+++ Zip.sql
@@ -1,5 +1,5 @@
-﻿CREATE TABLE Zip(
-	zipcode nvarchar(20) NOT NULL,
+CREATE TABLE Zip(
+	[Zipcode] nvarchar(20) NOT NULL,
 	City nvarchar(50) NOT NULL,
 	[State] nvarchar(20) NOT NULL,
-CONSTRAINT PK_zipcode PRIMARY KEY CLUSTERED (zipcode));
+CONSTRAINT PK_zipcode PRIMARY KEY CLUSTERED ([Zipcode]));
